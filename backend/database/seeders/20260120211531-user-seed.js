7be08230cf231b8cab476f13ee57f0d27221e648
jeevan-vaishnav <jeevanvaishnavv@gmail.com>
2026-01-21 11:01:34 +0000

    user created done

---
+++ backend/database/seeders/20260120211531-user-seed.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const { User } = require("../../app/models")
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    await User.create({
+      firstName: "Jeevan",
+      lastName: "Vaishnav",
+      email: "jeevan@gmail.com",
+      password: "123456"
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
--- backend/index.js
+++ backend/index.js
@@ -2,5 +2,4 @@ const Server = require('./server/index')
 const port = 8080
 const app = new Server(port);
 
-
 app.start()
